6eb68f520ce51467e107e2fb0e27a55fc81f84e2
mnagarjunam <64348732+mnagarjunam@users.noreply.github.com>
2020-12-17 11:03:30 +0530

    Add files via upload

---
+++ main/java/com/example/floatingactionbuttons/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.floatingactionbuttons;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton mAddFab, mAddAlarmFab, mAddPersonFab;
+
+    TextView addAlarmActionText, addPersonActionText;
+
+    Boolean isAllFabsVisible;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAddFab = findViewById(R.id.add_fab);
+        mAddAlarmFab = findViewById(R.id.add_alarm_fab);
+        mAddPersonFab = findViewById(R.id.add_person_fab);
+        addAlarmActionText = findViewById(R.id.add_alarm_action_text);
+        addPersonActionText = findViewById(R.id.add_person_action_text);
+        mAddAlarmFab.setVisibility(View.GONE);
+        mAddPersonFab.setVisibility(View.GONE);
+        addAlarmActionText.setVisibility(View.GONE);
+        addPersonActionText.setVisibility(View.GONE);
+
+        isAllFabsVisible = false;
+     mAddFab.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (!isAllFabsVisible) {
+
+                            mAddAlarmFab.show();
+                            mAddPersonFab.show();
+                            addAlarmActionText.setVisibility(View.VISIBLE);
+                            addPersonActionText.setVisibility(View.VISIBLE);
+                            isAllFabsVisible = true;
+                        } else {
+
+                            mAddAlarmFab.hide();
+                            mAddPersonFab.hide();
+                            addAlarmActionText.setVisibility(View.GONE);
+                            addPersonActionText.setVisibility(View.GONE);
+
+                            isAllFabsVisible = false;
+                        }
+                    }
+                });
+
+        mAddPersonFab.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this, "Person Added", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        mAddAlarmFab.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this, "Alarm Added", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
